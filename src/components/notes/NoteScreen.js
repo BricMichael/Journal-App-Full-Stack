46901dbe6644bb89893f7e9270c64b34d3ffed85
BricMichael <bricmichael@gmail.com>
2021-05-19 17:10:02 -0400

    #4. Home Page lista. Estilos con Sass completado.

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -6,6 +6,15 @@ const NoteScreen = () => {
         <div className="notes__main-content">
 
             <NotesAppBar />
+
+            <div className="notes__content">         
+                <input type="text" placeholder="Some awesome title" className="notes__title_input" autoComplete="off"></input>
+                <textarea placeholder="What happened today" className="notes__textarea"></textarea>
+            </div>
+
+            <div className="notes__image">
+                <img src="https://ugc.kn3.net/i/760x/http://img8.uploadhouse.com/fileuploads/15294/15294438b5ab0dd6dda2d9c190f9d7de4fa165e9.jpg" alt="event image"></img>
+            </div>
             
         </div>
     )
