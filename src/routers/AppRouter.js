65b3a346c15a16bd5402ceb37ce936577a4b31cc
BricMichael <bricmichael@gmail.com>
2021-05-23 15:37:37 -0400

    #08. Auth , Protegiendo las rutas.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import ScreenJournal from '../components/journal/ScreenJournal';
 import AuthRouter from './AuthRouter';
@@ -9,6 +9,11 @@ import { Login } from '../actions/auth';
 const AppRouter = () => {
 
     const dispatch = useDispatch();
+    const [ cheking, setCheking ] = useState(true);
+
+    const [ isLoggedIn, setIsLoggedIn ] = useState(false) // Privacidad a las rutas
+
+    
 
     useEffect(() => {
         // Funcion que permite estar a la escucha de las acciones de auth del user
@@ -16,12 +21,25 @@ const AppRouter = () => {
 
         if( user?.uid ){ //el signo de ? evalua si el user tiene algo y si es así, evalua el uid
             dispatch( Login( user.uid, user.displayName ) );
+            setIsLoggedIn(true) 
+        }else{
+            setIsLoggedIn(false)
         }
+        setCheking(false)
 
        });
 
 
-    }, [ dispatch ])
+    }, [ dispatch, setCheking, setCheking ]) //lo que usa el useEffect
+
+
+    if ( cheking ) {  //Vista de aviso de que se esta confirmando su auth
+        return (
+            <>
+                <h1>Wait...</h1>
+            </>
+        )
+    }
 
 
     return (
